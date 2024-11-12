a8ccfb5af9daba3b66a4a0fdf652bb5a8fa495d1
DevMic <nyakangomicah4@gmail.com>
2024-11-12 18:37:21 +0300

    js database

---
+++ assets/js/person.js
@@ -0,0 +1,10 @@
+export const person = [
+    {
+        fName:'MICAH',
+        lNmae:'MOSOMI',
+        userName:'MICDEV',
+        email:'nyakangomicah4@gmail.com',
+        password:12345678
+
+    }
+]
